a377b4c07c00067cf99b2550c8f39bab07cc17bc
HONG ZHOU <holly_zhou@yahoo.com>
2016-08-22 12:24:35 -0700

    continue

--- server/routes/stocks.js
+++ server/routes/stocks.js
@@ -1,30 +1,22 @@
 const express = require('express');
 const router = express.Router();
-// const jsonp = require('jsonp');
-// const fetchJsonp = require('fetch-jsonp');
-const request = require('request');
+const jsonp = require('jsonp');
+
+// const request = require('request');
 
 router.route('/:symbol')
  .get((req, res) =>{
-   console.log('hit route');
-  request(`http://dev.markitondemand.com/MODApis/Api/Lookup/jsonp?input=${req.params.symbol}`,
-   function(err, response, body) {
-     if(!err && response.statusCode == 200 ) {
-       let newObj = JSON.parse(body)
-       console.log('newObj', newObj);
+  jsonp(`http://dev.markitondemand.com/MODApis/Api/Lookup/jsonp?input=${req.params.symbol}`,
+    function(err, data) {
+     if(err){
+       console.error(err.message);
+     }else{
+       console.log(data);
      }
-     res.send(body);
-      })
+   });
 });
 
 
 
- //     if(err) cb(err);
- //     if(!err){
- //       let companyData = JSON.parse(data)
- //       cb(null, companyData)
- //     }
- //   })
- // })
 
 module.exports = router;
